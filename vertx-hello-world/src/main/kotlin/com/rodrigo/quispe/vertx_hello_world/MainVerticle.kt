d7f73edd4a241176207b9ce3289f2d04a6379358
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-02-20 16:31:11 -0500

    Live Redeploy
    run com.rodrigo.quispe.vertx_hello_world.MainVerticle --redeploy=**/*.class --launcher-class=io.vertx.core.Launcher

--- vertx-hello-world/src/main/kotlin/com/rodrigo/quispe/vertx_hello_world/MainVerticle.kt
+++ vertx-hello-world/src/main/kotlin/com/rodrigo/quispe/vertx_hello_world/MainVerticle.kt
@@ -3,8 +3,11 @@ package com.rodrigo.quispe.vertx_hello_world
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
 import io.vertx.core.Vertx
+import org.slf4j.LoggerFactory
 
 class MainVerticle : AbstractVerticle() {
+
+  private val logger = LoggerFactory.getLogger(MainVerticle::class.java)
 
   override fun start(startPromise: Promise<Void>) {
     vertx
@@ -17,7 +20,8 @@ class MainVerticle : AbstractVerticle() {
       .listen(8888) { http ->
         if (http.succeeded()) {
           startPromise.complete()
-          println("HTTP server started on port 8888")
+          logger.debug("HTTP server started on port 8888")
+          logger.debug("Done")
         } else {
           startPromise.fail(http.cause());
         }
