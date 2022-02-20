8fd894bb9819a0caa034769804d04e4c900fdc0a
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-02-20 17:39:50 -0500

    Vert.x Web Application

--- vertx-stock-broker/build.gradle.kts
+++ vertx-stock-broker/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
   implementation("io.vertx:vertx-web")
   implementation("io.vertx:vertx-lang-kotlin")
   implementation(kotlin("stdlib-jdk8"))
+  implementation("ch.qos.logback:logback-classic:1.2.10")
   testImplementation("io.vertx:vertx-junit5")
   testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 }
--- vertx-stock-broker/src/main/kotlin/com/rodrigo/quispe/vertx_stock_broker/MainVerticle.kt
+++ vertx-stock-broker/src/main/kotlin/com/rodrigo/quispe/vertx_stock_broker/MainVerticle.kt
@@ -2,8 +2,12 @@ package com.rodrigo.quispe.vertx_stock_broker
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
+import io.vertx.core.Vertx
+import org.slf4j.LoggerFactory
 
 class MainVerticle : AbstractVerticle() {
+
+  private val logger = LoggerFactory.getLogger(MainVerticle::class.java)
 
   override fun start(startPromise: Promise<Void>) {
     vertx
@@ -16,10 +20,23 @@ class MainVerticle : AbstractVerticle() {
       .listen(8888) { http ->
         if (http.succeeded()) {
           startPromise.complete()
-          println("HTTP server started on port 8888")
+          logger.info("HTTP server started on port 8888")
         } else {
           startPromise.fail(http.cause());
         }
       }
+  }
+}
+
+fun main() {
+  val vertx = Vertx.vertx()
+  vertx.exceptionHandler { error ->
+    println("UNHANDLED: $error")
+  }
+  vertx.deployVerticle(MainVerticle()) { ar ->
+    if (ar.failed()) {
+      println("FAILED_DEPLOY: ${ar.cause()}")
+    }
+    println("DEPLOYED MainVerticle!")
   }
 }
