cbeb33ad6fe3fe04fc00c28feaa769b5653d6ff0
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-05 19:40:37 -0500

    refactor

--- vertx-quarkus-reactive/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users(id, name) VALUES (nextval('hibernate_sequence'), 'Alice');
-INSERT INTO users(id, name) VALUES (nextval('hibernate_sequence'), 'Bob');
-INSERT INTO users(id, name) VALUES (nextval('hibernate_sequence'), 'Charlie');
